4b0bee9099a9d06722457e15482ca739594ce4ac
Lobay <32384708+Lobay@users.noreply.github.com>
2017-11-13 15:04:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int i;
-    int k; 
-    int z;
-    cin >> k;
-    if (k <= 0){
-        return 0;
-    }
-    int a[k];
-    for (i = 0; i < k; i++) {
-        cin >> a[i];
-    }
-    cin >> z;
-    int b[z - 1], c[k - z - 1];
-    for (i = 0; i < z; i++) {
-        b[i] = a[i];
-    }
-    for (i = 0; i < k - z; i++) {
-        c[i] = a[i + z];
-    }
-    for (i = 0; i < k - z; i++) {
-        cout << c[i] << " ";
-    }
-    for (i = 0; i < z; i++) {
-        cout << b[i] << " ";
-    }
-    return 0;
+	int i;
+	int k; 
+	int z;
+	cin >> k;
+	if (k <= 0){
+	cout << "An error has occurred while reading input data" << endl;
+		return 0;
+	}
+	int *a = new int[k];
+	for (i = 0; i < k; i++) {
+		cin >> a[i];
+	}
+	cin >> z;
+	if (z <= 0){
+		cout << "An error has occurred while reading input data" << endl;
+		return 0;
+	}
+	int b[ z ], c[ k - z ];
+	for (i = 0; i < z; i++) {
+		b[i] = a[i];
+	}
+	for (i = 0; i < k - z; i++) {
+		c[i] = a[i + z];
+	}
+	for (i = 0; i < k - z; i++) {
+		cout << c[i] << " ";
+	}
+	for (i = 0; i < z; i++) {
+		cout << b[i] << " ";
+	}
+	return 0;
 }
